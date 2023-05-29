2966613f0f5e11be1b3537f0770e253196acced8
Catalina Hernandez <cataxto@gmail.com>
2023-05-29 14:16:11 -0500

    Primeras rutas - enrutado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Header from "./components/Header";
-import { Home } from "./components/Home";
+import Home  from "./components/Home";
 import SearchPage from "./components/SearchPage";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header/>
-      <Home/>
-      <SearchPage/>
-    </div>
+      <Routes>
+
+        <Route path="/" element={<Home/>}/>
+        <Route path="/search" element={<SearchPage/>}/>
+
+        
+      </Routes>
+    </Router>
   );
 }
 
