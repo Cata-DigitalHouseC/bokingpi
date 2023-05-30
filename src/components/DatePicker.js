40a78a623176a10899a70cf138a07493c9e8ff91
Catalina Hernandez <cataxto@gmail.com>
2023-05-29 19:28:18 -0500

    Configurando el DatePicker, con opciones adicionales

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,7 +1,9 @@
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css'
 import {DateRangePicker} from 'react-date-range'
-import { makeStyles } from '@material-ui/core';
+import { Button, InputBase, Typography, makeStyles } from '@material-ui/core';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import {useNavigate} from "react-router-dom"
 
 
 const selectionRange = {
@@ -11,12 +13,23 @@ const selectionRange = {
 }
 const DatePicker = () => {
   const classes = useStyle();
+  const history = useNavigate();
   const handleSelect = () => {}  
   return (
     <div className = {classes.root}>
         <DateRangePicker ranges={[selectionRange]}
             onChange={handleSelect}
         />
+        <div className = {classes.inputSection}>
+            <Typography variant="h5">Number of guests</Typography>
+            <div className = {classes.people}>
+                <InputBase placeholder = "2"
+                    inputProps={{className: classes.input}}
+                />
+                <PeopleAltIcon/>
+            </div>
+            <Button onClick={()=> history("/search")}>Search Space to work!</Button>
+        </div>
     </div>
   )
 }
@@ -30,6 +43,31 @@ const useStyle=makeStyles((theme)=>({
             top:"16vh",
             left:0
         }
+    },
+    inputSection:{
+        display:"flex",
+        flexDirection:"column",
+        backgroundColor:"#fff",
+        "& h5":{
+            textAlign:"center"
+        },
+        "& button:hover":{
+            backgroundColor:"rgba(255,103,31,0.4)",
+            color:"#fff"
+        }
+    },
+    people:{
+        display:"flex",
+        justifyContent: "center",
+        alignItems:"center",
+
+    },
+    input:{
+        width:"3vw",
+        border:"1px solid #ccc",
+        margin: theme.spacing(1,1,1,3),
+        padding: theme.spacing(1,0,1,3),
+        justifyContent:"center"
     }
 }))
 
