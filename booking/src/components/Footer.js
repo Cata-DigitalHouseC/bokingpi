06516618e7500d77465a5237cf78f6e737f47147
Catalina Hernandez <cataxto@gmail.com>
2023-05-29 23:18:27 -0500

    trabajando en el SearchPage.js

--- booking/src/components/Footer.js
+++ booking/src/components/Footer.js
@@ -1,3 +1,4 @@
+import { Typography, makeStyles } from '@material-ui/core'
 import React from 'react'
 
 const Footer = () => {
--- booking/src/components/SearchPage.js
+++ booking/src/components/SearchPage.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import { Chip, Slider, Typography, makeStyles } from '@material-ui/core'
+import mockData,{chips} from "../mockData"
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 const SearchPage = () => {
+    const classes = useStyle()
   return (
-    <div>
-        <h1>Search</h1>
+    <div className={classes.root}>
+        <Typography variant="h5" gutterBottom>
+            Available Spaces
+        </Typography>
+        <div>
+            {
+                chips.map(data=>{
+                    let icon = <HighlightOffIcon/>
+                    return(
+                        <Chip label = {data.label} key={data.key} icon={icon} className = {classes.chip}/>
+                    )
+                })
+            }
+        </div>
+        <div className={classes.selector}>
+            <Typography gutterBottom>Prices</Typography>
+            <Slider value= onChange={}
+                aria-labelledby='="continuos-slider'
+                min={100}
+                max={400}
+                valueLabelDisplay="auto"
+            />
+            
+        </div>
+
     </div>
   )
 }
+
+const useStyle=makeStyles((theme)=>({
+    root:{},
+    chips:{
+        padding:theme.spacing(1),
+    },
+    chip:{
+        padding:theme.spacing(1),
+    },
+    selector:{
+        width:"300px",
+        marginLeft: theme.spacing(3)
+    }
+
+}))
 
 export default SearchPage
